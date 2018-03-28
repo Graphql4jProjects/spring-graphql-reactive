c920c72a030f17982e8f248e4e4b2a3441448e39
jRoadie <akter.hossain.ict@gmail.com>
2018-03-28 15:49:35 +0600

    attache graphiql autoconfig

--- src/main/java/graphql4j/springframework/graphql/SimpleGraphiqlController.java
+++ src/main/java/graphql4j/springframework/graphql/SimpleGraphiqlController.java
@@ -1,6 +1,7 @@
 package graphql4j.springframework.graphql;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @ConditionalOnMissingBean(GraphiqlController.class)
+@ConditionalOnProperty(name = "spring.graphiql.url")
 @RestController
-@RequestMapping("${spring.graphiql.url:/graphiql}")
+@RequestMapping("${spring.graphiql.url}")
 public class SimpleGraphiqlController implements GraphiqlController {
 
   @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
